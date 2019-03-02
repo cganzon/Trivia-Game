29a66f9e15e6da065af6a162f9c6ea1ea6e7d803
cganzon <crganzon@gmail.com>
2019-03-01 23:38:20 -0800

    worked on scores functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,13 +44,9 @@ function run() {
     interval = setInterval(decrement, 1000);
     $("#questions").empty();
     $("#questions").html('<p>Time remaining: <span id="timer">30</span> seconds</p>');
-    for (var i = 0; i < qA.length; i++) {
-        $("#questions").append("<p>" + qA[i].q + "</p>");
-            $("#questions").append('<input type="radio" name=' + qA[i].name + ' value=' + qA[i].options[0] + '> ' + qA[i].options[0]);
-            $("#questions").append('<input type="radio" name=' + qA[i].name + ' value=' + qA[i].options[1] + '> ' + qA[i].options[1]);
-            $("#questions").append('<input type="radio" name=' + qA[i].name + ' value=' + qA[i].options[2] + '> ' + qA[i].options[2]);
-    }
+    makeQuestions();
     $("#done").show();
+
 }
 
 function decrement() {
@@ -61,7 +57,7 @@ function decrement() {
         alert("Time's Up!");
     }
 }
-
+//  Function to show the user's scores
 function done() {
     clearInterval(interval);
     counter = 30;
@@ -72,5 +68,41 @@ function done() {
     $("#questions").append("<p> Incorrect: " + incorrect + "</p>");
     $("#questions").append("<p> Unanswered: " + unanswered + "</p>");
 }
+
+// Function to generate questions on page
+function makeQuestions() {
+    for (var i = 0; i < qA.length; i++) {
+        $("#questions").append("<p>" + qA[i].q + "</p>");
+        $("#questions").append('<input type="radio" name=' + qA[i].name + ' value=' + qA[i].options[0] + '> ' + qA[i].options[0]);
+        $("#questions").append('<input type="radio" name=' + qA[i].name + ' value=' + qA[i].options[1] + '> ' + qA[i].options[1]);
+        $("#questions").append('<input type="radio" name=' + qA[i].name + ' value=' + qA[i].options[2] + '> ' + qA[i].options[2]);
+        console.log(qA[i].name);
+        // Function to check answers
+        function checkAnswers() {
+            $("input").on("click", function () {
+                if ($("input[name=" + qA[i].name + "]:checked")) {
+                    console.log("Checked!");
+                }
+
+            });
+        checkAnswers();
+        }
+        // // Function to check answers
+        // function checkAnswers() {
+        //     // var isChecked = $("input[name=" + qA[i].name + "]:checked");
+        //     console.log(qA[i].name);
+        // }
+        // checkAnswers();
+    }
+}
+// Function to check answers
+// function checkAnswers() {
+//     var isChecked = $("input[name=" + qA[i].name + "]:checked");
+//     for (var i = 0; i < qA.length; i++) {
+//         var isChecked = $("input[name=" + qA[i].name + "]:checked");
+//         console.log(isChecked);
+        // if ($(qA[i].name.value).
+//     }
+// }
 
 
